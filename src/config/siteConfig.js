5303f0b232f89bcfd52a361a40c02a1ac279a5c9
Atif Mohammed <Atif089@gmail.com>
2025-06-10 22:56:40 -0500

    added video URLs

--- src/config/siteConfig.js
+++ src/config/siteConfig.js
@@ -29,16 +29,22 @@ export const siteConfig = {
       image: "/hip-replacement-1.png",
       title: "Hip Replacement",
       text: "Experience renewed mobility and relief with our advanced, minimally invasive hip replacement approach.",
+      videoUrl:
+        "https://player.understand.com/ucomortho2024/en/a9a23aad-281c-45d6-afcd-80ad4cfa2502/3e5caddb-4e86-422c-925d-3e3662646360",
     },
     {
       image: "/knee-replacement-2.png",
       title: "Knee Replacement",
       text: "Regain your stride and comfort through precise, patient-focused knee replacement surgery.",
+      videoUrl:
+        "https://player.understand.com/ucomortho2024/en/7f4aed12-70dc-453f-a6da-9824f590590c/79f35f43-9039-4647-bcea-b85bc3fc87fd",
     },
     {
       image: "/shoulder-replacement-3.png",
       title: "Total Shoulder Replacement",
       text: "Restore strength and ease of movement with expert shoulder joint replacement tailored to your needs.",
+      videoUrl:
+        "https://player.understand.com/ucomortho2024/en/87527a46-1c59-4576-a311-85e4fb88ed1e/f12a6da1-a387-4cff-803a-b0aa2ca7e0d6",
     },
   ],
 };
